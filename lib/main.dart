1032fde91bdff0be19d7ced84e926069aa51d4f6
Riaz Uddin <riaz.uddin27@gmail.com>
2023-06-07 17:50:11 +0600

    Firebase configured
    Used flutterfire cli for configuring project with Firebase database.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'dart:ui';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:visoattend/firebase_options.dart';
 
 import './views/pages/login_page.dart';
 import 'global_bindings.dart';
 
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   DartPluginRegistrant.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   // GlobalBinding().dependencies();
   runApp(
     GetMaterialApp(
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_auth
+import firebase_core
 import isar_flutter_libs
 import path_provider_foundation
 import shared_preferences_foundation
 import tflite_flutter_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
